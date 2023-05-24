9bbf95162b22f2543aac6a34bf1ec0499b77e8d1
Ebubekir <ebubekirc442@gmail.com>
2023-05-24 13:43:41 +0300

    commit

--- src/main/java/com/tpe/repo/LocalRepo.java
+++ src/main/java/com/tpe/repo/LocalRepo.java
@@ -5,6 +5,7 @@ import com.tpe.domain.Course;
 public class LocalRepo implements Repo{
     @Override
     public void save(Course course) {
+        System.out.println("------------------");
         System.out.println(course.getName() + " DERSİ LOCAL'E KAYDEDİLİYOR !!");
     }
 }
