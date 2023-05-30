8cbb443f17ee5ab4ea73e6a20cc58dbd61d58ec6
Ebubekir <ebubekirc442@gmail.com>
2023-05-30 14:32:02 +0300

    commit

---
+++ src/main/java/com/tpe/domain/Course.java
@@ -0,0 +1,17 @@
+package com.tpe.domain;
+
+public class Course {
+
+
+    private String name;
+
+    //!!! GETTER-SETTER
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
